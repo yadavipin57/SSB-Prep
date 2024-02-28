825599d61f2e28bf0e1e63d20d3a9392d837ad45
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-02-28 12:02:35 +0000

    Updated

---
+++ psychology/psychology.js
@@ -0,0 +1,12 @@
+const anchor = document.querySelectorAll('.section a')
+
+const tat = document.querySelector('.tat')
+const wat = document.querySelector('.wat')
+const srt = document.querySelector('.srt')
+const sdt = document.querySelector('.sdt')
+
+anchor.forEach((card)=>{
+    card.addEventListener('click', ()=>{
+        
+    })
+})
