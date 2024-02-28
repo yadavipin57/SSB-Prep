e6f39fd5982dc723ed52e31c87bd7ddb9bc7c470
Vipin Yadav <63735356+yadavipin57@users.noreply.github.com>
2024-02-28 16:13:48 +0000

    Changes made

--- psychology/psychology.js
+++ psychology/psychology.js
@@ -5,8 +5,25 @@ const wat = document.querySelector('.wat')
 const srt = document.querySelector('.srt')
 const sdt = document.querySelector('.sdt')
 
-anchor.forEach((card)=>{
-    card.addEventListener('click', ()=>{
-        
-    })
+
+// Start Wat function
+
+const startButton = document.querySelector('.start-button')
+
+const watWords = ['Table', 'Chair', 'Fan', 'Lion', 'Dog', 'Cat']
+
+startButton.addEventListener('click', ()=>{
+    const startDiv = document.querySelector('.start')
+    startDiv.style.display = 'none'
+    startWat();
 })
+
+let watIndex = 0
+
+function startWat(){
+    setInterval(()=>{
+        const watContentWords = document.querySelector('.wat-content p')
+        watContentWords.innerHTML = watWords[watIndex]
+        watIndex++;
+    }, 5000)
+}
